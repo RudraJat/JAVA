16d368a1f8c2275ceeefeae4f0d0a31d8fe0c5b7
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-01-28 17:56:25 +0530

    input output java

--- First.java
+++ First.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class First{
     public static void main(String[] args) {
         System.out.println("Hello World! ");
         System.out.println("My name is Rudra");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextInt());
+        input.close();
     }
 }
