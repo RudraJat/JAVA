f4b6a9fa0f2b7b45f4bbcd20dd2849745b72e15f
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-01-28 18:14:41 +0530

    up

--- datatype.java
+++ datatype.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class datatype {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter input: ");
         int rollno = input.nextInt();
         System.out.println("Your roll no. is: "+rollno);
         input.close();
