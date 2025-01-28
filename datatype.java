53cb6935b376d1f31e867bb8efc64fc12352ed1e
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-01-28 18:08:37 +0530

    input/ouput

---
+++ datatype.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class datatype {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int rollno = input.nextInt();
+        System.out.println("Your roll no. is: "+rollno);
+        input.close();
+    }
+}
