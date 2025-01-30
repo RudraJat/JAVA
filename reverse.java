915063e52f515020d26ce2a8ffd92dd90a5d7cdd
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-01-30 22:08:52 +0530

    reversing number

---
+++ reverse.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class reverse {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        long n = in.nextLong();
+        long rev = 0;
+        
+        while(n!=0){
+            long rem=n%10;
+            rev=rev*10+rem;
+            n/=10;
+        }
+        System.out.println(rev);   
+    }
+}
