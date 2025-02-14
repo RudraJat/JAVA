e3a0fb3a562b60db95c399b63b2878a157610d2e
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-02-14 15:42:09 +0530

    up

---
+++ armstrong.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class armstrong {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+        printArmstrong(arr);
+    }
+    static void printArmstrong(int[] arr){
+        for(int num:arr){
+            if(isArmstrong(num)){
+                System.out.println(num);
+            }
+        }
+    }
+    
+    static boolean isArmstrong(int num){
+        int temp = num, sum=0, dig=0;
+        int n=num;
+
+        //count digits
+        while(n>0){
+            dig++;
+            n/=10;
+        }
+        n=num;
+
+        while(n>0){
+            int rem=n%10;
+            sum+=Math.pow(rem,dig);
+            n/=10;
+        }
+        return sum==temp;
+    }
+}
---
+++ prime.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class prime {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+
+        int n=in.nextInt();
+        int[] arr=new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+
+        printPrime(arr);
+    }
+    static void printPrime(int[] arr){
+        for(int num: arr){
+            if(isPrime(num)){
+                System.out.print(num+" ");
+            }
+        }
+    }
+    static boolean isPrime(int num){
+        if(num<=1){
+            return false;
+        }
+            for(int j=2;j*j<=num;j++){
+                if(num%j==0){
+                    return false;
+                }
+        }       
+        return true;
+    }
+}
